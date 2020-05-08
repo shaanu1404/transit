f7092b5f226b3af50600cda88db126bf38959f8c
shaanu1404 <shanu14tnt@gmail.com>
2020-05-08 17:49:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (request, response, next) => {
+  response.sendFile(path.join(__dirname, 'template/index.html'));
+});
+
+app.get('/generic', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'template/generic.html'));
+});
+
+app.get('/elements', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'template/elements.html'));
+});
+
+const PORT =  process.env.port || 5000;
+
+app.listen(PORT, () => console.log('Server started in port ' + PORT));
